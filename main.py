392a4dcf7326b39e58ac5f40668566e4cbac7f37
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2024-11-29 14:52:58 +0000

    Fixed Plotting Issue

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyaudio
 import wave
+import sys
 
 
 def retreiveFile(filename):
@@ -9,6 +10,7 @@ def retreiveFile(filename):
     chunk = 2048
 
     try:
+        # Opening the I/O
         # 'rb' means to read-only
         wf = wave.open(filename, 'rb')
         p = pyaudio.PyAudio()
@@ -19,7 +21,6 @@ def retreiveFile(filename):
                         rate=wf.getframerate(),
                         output=True)
 
-
         plt.ion()  # Keep it interactive as you won't be able to statically redraw the plot for each frame
         fig, ax = plt.subplots()
         line, = ax.plot([], [], lw=2)
@@ -27,7 +28,7 @@ def retreiveFile(filename):
         ax.set_xlabel("Time (seconds)")
         ax.set_ylabel("Amplitude")
         ax.set_xlim(0, chunk / wf.getframerate())  # Set initial x-axis limits
-        ax.set_ylim(-2 ** 15, 2 ** 15 - 1)  # Set y-axis limits for 16-bit audio
+        ax.set_ylim(0, 2 ** 15 - 1)  # Set y-axis limits for 16-bit audio
 
         try:
             data = wf.readframes(chunk)
@@ -50,13 +51,13 @@ def retreiveFile(filename):
                 ax.set_xlim(time[0], time[-1])  # Dynamically adjust x-axis
 
                 # Redraw the plot
-                plt.draw()
                 plt.pause(0.01)  # Small pause to allow the plot to update
 
                 total_frames_played += len(signal)
 
                 data = wf.readframes(chunk)
 
+            plt.draw()
         except KeyboardInterrupt:
             print("Playback interrupted.")
         except Exception as e:
@@ -66,14 +67,14 @@ def retreiveFile(filename):
         p.terminate()
         plt.ioff()
         plt.show()
-    except Exception as e:
+    except FileNotFoundError as e:
         (print("you may have an issue with the file name"))
-
 
 
 # This is just an example
 # Download a wav file or convert to a wav file and copy the path as the parameter
-retreiveFile("D:\\Google downloads\\showreel-music-promo-advertising-opener-vlog-background-intro-theme-261601.wav")
+if __name__ == "__main__":
+    retreiveFile("D:\\Google downloads\\showreel-music-promo-advertising-opener-vlog-background-intro-theme-261601.wav")
 
 # Issues currently, It's VERY loud Another is when amplitudes start peaking the speed of frames being read drops
 # leading to choppy audio and slow graph drawing Maybe solve via multi threading, have 1 core play sound and another
